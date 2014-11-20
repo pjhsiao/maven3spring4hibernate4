0903d58881641b6278dba3bf0002ff64d087c3f4
pjhsiao <pjhsiao99@gmail.com>
2014-11-20 10:35:28 +0800

    註解說明
    Signed-off-by: pjhsiao <pjhsiao99@gmail.com>

--- src/main/java/hsiao/spring4hibernate4/aop/Audience.java
+++ src/main/java/hsiao/spring4hibernate4/aop/Audience.java
@@ -5,15 +5,17 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-@Aspect
+@Aspect//定義一個切面
 public class Audience {
 	/**
+	 * 定義一個切點
 	 * define Pointcut
 	 */
 	@Pointcut("execution(* hsiao.spring4hibernate4.dao.impl.*.*(..))") // expression
 	public void adviceMethod(){
 		
 	}
+	//定義一個通知Before
 	@Before("adviceMethod()")
 	public void doBeforeTask(){
 		System.out.println("		─▀▀▀▀▀▀█▀▀▀▀▀▀──────▄▄");
@@ -22,7 +24,7 @@ public class Audience {
 		System.out.println("		█████████████▀");
 		System.out.println("		─▄▄█▄▄▄▄▄▄▄█▄▄ ★○ ★ . *:. *. ° ○ °.. *.☾° *♥¸　.　☾ °☆. • *¨ `* •");
 	}
-	
+	//定義一個通知After
 	@After("adviceMethod()")
 	public void doAfterTask(){
 		System.out.println("		█◣　　　█　　　　　　　◢█ ");
